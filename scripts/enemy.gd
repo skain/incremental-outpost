23f0744ae7b266a4d89a3f31b1999051def139f4
skain <stevekain@gmail.com>
2026-01-12 10:27:57 -0600

    enemy timer respawn, scale projectile from root node

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,7 +1,11 @@
 extends Node2D
 class_name Enemy
 
+@export var hit_timeout_secs: int = 5
+
+
 @onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var timer: Timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,9 +25,15 @@ func _disable() -> void:
 	print('disabled')
 	visible = false
 	collision_shape_2d.set_deferred("disabled", true)
+	timer.start(hit_timeout_secs)
+	
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.name == "PlayerProjectile":
 		print('hit')
 		_disable()
 		area.queue_free()
+
+
+func _on_timer_timeout() -> void:
+	_enable()
