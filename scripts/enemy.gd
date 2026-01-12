8d8ee33e8b437562d46d193cec8557bb21fff359
skain <stevekain@gmail.com>
2026-01-12 10:20:46 -0600

    Enemy disabling on hit

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name Enemy
 
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -10,8 +12,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _enable() -> void:
+	print('enabled')
+	visible = true
+	collision_shape_2d.set_deferred("disabled", false)
+	
+func _disable() -> void:
+	print('disabled')
+	visible = false
+	collision_shape_2d.set_deferred("disabled", true)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.name == "PlayerProjectile":
 		print('hit')
+		_disable()
 		area.queue_free()
