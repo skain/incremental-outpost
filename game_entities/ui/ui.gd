5d0c95a5363ae8583bf6f6ca8362a936061b4a1e
skain <stevekain@gmail.com>
2026-02-06 22:53:12 -0600

    start working on upgrade ui

--- game_entities/ui/ui.gd
+++ game_entities/ui/ui.gd
@@ -3,19 +3,19 @@ class_name UI extends Node2D
 
 @onready var game_over_panel: Panel = %GameOverPanel
 @onready var arcade_overlay: ArcadeOverlay = %ArcadeOverlay
-@onready var upgrade_interface: Node2D = %UpgradeInterface
+@onready var upgrade_interface: UpgradeInterface = %UpgradeInterface
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:	
-	print("Panel Size: ", game_over_panel.size)
-	print("Viewport Size: ", get_viewport_rect().size)
+func _ready() -> void:
 	hide_game_over()
 
 func update_ui(score: int, player_health: int) -> void:
 	arcade_overlay.update_ui(score, player_health)
 
 func show_game_over() -> void:
-	upgrade_interface.visible = true
+	upgrade_interface.show_upgrade_interface()
+	upgrade_interface.make_camera_current()
 	
 func hide_game_over() -> void:
 	upgrade_interface.visible = false
+	upgrade_interface.hide_upgrade_interface()
