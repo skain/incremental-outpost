639f4b6311bcda1cc339fe4201e538357eaec913
skain <stevekain@gmail.com>
2026-02-20 13:38:38 -0600

    remove dev world env

--- scenes/skill_tree/skill_tree.gd
+++ scenes/skill_tree/skill_tree.gd
@@ -3,13 +3,13 @@ class_name SkillTree extends Node2D
 @onready var nodes: Node2D = %Nodes
 @onready var lines: Node2D = %Lines
 @onready var camera_2d: Camera2D = %Camera2D
-@onready var dev_world_environment: WorldEnvironment = %DevWorldEnvironment
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if not Engine.is_editor_hint():
-		if dev_world_environment:
-			dev_world_environment.queue_free()
+	pass
+	#if not Engine.is_editor_hint():
+		#if dev_world_environment:
+			#dev_world_environment.queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
