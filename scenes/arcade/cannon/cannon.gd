b25963f4d432159e3ac26a36de38b09b740aa008
skain <stevekain@gmail.com>
2026-02-20 16:49:38 -0600

    moved more stuff

--- scenes/arcade/cannon/cannon.gd
+++ scenes/arcade/cannon/cannon.gd
@@ -3,7 +3,7 @@ class_name Cannon
 
 signal cannon_hit(cannon_direction: Vector2)
 
-const PROJECTILE_SCENE = preload("res://scenes/player_projectile/player_projectile.tscn")
+const PROJECTILE_SCENE = preload("res://scenes/arcade/player_projectile/player_projectile.tscn")
 
 @onready var sprite_2d: Sprite2D = $Cannon
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
