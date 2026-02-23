d768e3c6816ff2f42c3d345e6d8cb0408820d635
Steve Kain <stevekain@gmail.com>
2026-02-23 14:53:51 -0600

    skill tree line working

--- scenes/crt/crt_panel/skill_tree_nodes.gd
+++ scenes/crt/crt_panel/skill_tree_nodes.gd
@@ -7,4 +7,5 @@ func _ready() -> void:
 	root_node = get_child(0) as SkillTreeNode
 
 func home_camera() -> void:
+	skill_tree_camera.zoom = Vector2.ONE
 	skill_tree_camera.global_position = root_node.global_position
--- scenes/crt/skill_tree_node/skill_tree_node.gd
+++ scenes/crt/skill_tree_node/skill_tree_node.gd
@@ -1,1 +1,33 @@
-class_name SkillTreeNode extends Node2D
+class_name SkillTreeNode extends Sprite2D
+
+#func _ready() -> void:
+	#for node in get_children():
+		#var skill_node := node as SkillTreeNode
+		#var line := Line2D.new()
+		#
+		#line.add_point(Vector2.ZERO)
+		#line.add_point(skill_node.position)
+		#
+		#line.default_color = Color(1.0, 1.0, 1.0, 1.0)
+		#line.width = 2.0
+		##line.top_level = true
+		#
+		#add_child(line)
+		
+func _ready() -> void:
+	for node in get_children():
+		var skill_node := node as SkillTreeNode
+		if not skill_node: 
+			continue # Good practice: skips the node if it isn't a SkillTreeNode
+			
+		var line := Line2D.new()
+
+		# Vector2.ZERO is (0,0), which is exactly the center of the parent
+		line.add_point(Vector2.ZERO) 
+		line.add_point(skill_node.position)
+		line.z_index = -1
+
+		line.default_color = Color(1.0, 1.0, 1.0, 1.0)
+		line.width = 2.0
+
+		add_child(line)
