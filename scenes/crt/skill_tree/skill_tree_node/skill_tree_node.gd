d0304f4209a197483150a39f58f7c85ad95069c9
Steve Kain <stevekain@gmail.com>
2026-03-05 11:59:02 -0600

    working on state management for nodes, need to work on node and lines updates next

--- scenes/crt/skill_tree/skill_tree_node/skill_tree_node.gd
+++ scenes/crt/skill_tree/skill_tree_node/skill_tree_node.gd
@@ -1,24 +1,26 @@
 class_name SkillTreeNode extends Sprite2D
 
+enum SkillNodeStatus {UNREVEALED, UNAFFORDABLE, AFFORDABLE, PURCHASED}
+
 signal skill_tree_node_clicked(node: SkillTreeNode)
 
 @export var skill_node_resource: SkillNodeResource
 
-var current_level := 1
+const COLOR_MODULATOR_UNAVAILABLE := Color(1.0, 1.0, 1.0, 0.0)
+const COLOR_MODULATOR_UNAFFORDABLE := Color(0.75, 0.75, 0.75, 1.0)
+const COLOR_MODULATOR_AFFORDABLE := Color(1.75, 1.75, 1.75, 1.0)
+
+var _current_status := SkillNodeStatus.UNREVEALED
 
 
 func _ready() -> void:
 	_validate_skill_node_resource()
 	_register_with_game_manager()
-	draw_lines()
+	_draw_lines()
 	
 	
-func draw_lines() -> void:
-	for node in find_children("*", "SkillTreeNode", true, false):
-		var skill_node := node as SkillTreeNode
-		if not skill_node: 
-			continue # Good practice: skips the node if it isn't a SkillTreeNode
-			
+func _draw_lines() -> void:
+	for skill_node in _get_child_skill_nodes():			
 		var line := Line2D.new()
 
 		# Vector2.ZERO is (0,0), which is exactly the center of the parent
@@ -27,28 +29,54 @@ func draw_lines() -> void:
 		line.z_index = -1
 
 		line.default_color = Color(1.0, 1.0, 1.0, 1.0)
+		line.self_modulate = COLOR_MODULATOR_UNAVAILABLE
 		line.width = 2.0
 
 		add_child(line)
 
 
-func get_current_cost() -> int:
-	#placeholder
-	return current_level * skill_node_resource.skill_cost
+func get_cost() -> int:
+	return skill_node_resource.skill_cost
 	
 	
-func get_current_modifier_value() -> float:
-	return skill_node_resource.modifier_values_by_level[current_level]
+func get_modifier_value() -> float:
+	return skill_node_resource.modifier_value
 	
-func try_raise_level() -> void:
-	if current_level < skill_node_resource.max_level:
-		current_level += 1
-		
+func update_from_game_data() -> void:
+	_update_node_appearance_from_game_data()
+	_update_lines_appearance_from_game_data()
 
+
+func _update_node_appearance_from_game_data() -> void:
+	pass
+	
+	
+func _update_lines_appearance_from_game_data() -> void:
+	pass
+	
+	
 func _register_with_game_manager() -> void:
 	GameManager.register_skill_node(self)
 	
 
+func _get_child_skill_nodes() -> Array[SkillTreeNode]:
+	var arr: Array[SkillTreeNode] = []
+	for node in find_children("*", "SkillTreeNode", true, false):
+		var skill_node := node as SkillTreeNode
+		if skill_node: 
+			arr.append(skill_node)
+		
+	return arr
+	
+func _get_child_lines() -> Array[Line2D]:
+	var arr: Array[Line2D] = []
+	for node in find_children("*", "Line2D", true, false):
+		var line := node as Line2D
+		if line: 
+			arr.append(line)
+	
+	return arr
+	
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		skill_tree_node_clicked.emit(self)
@@ -56,7 +84,4 @@ func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int
 
 func _validate_skill_node_resource() -> void:
 	assert(skill_node_resource != null)
-	assert(skill_node_resource.max_level > 0)
-	assert(skill_node_resource.skill_cost >= 0)
 	assert(skill_node_resource.skill_name != null and skill_node_resource.skill_name != "")
-	assert(skill_node_resource.max_level == len(skill_node_resource.modifier_values_by_level))
--- scripts/skill_node_resource.gd
+++ scripts/skill_node_resource.gd
@@ -6,11 +6,6 @@ enum ModifierType { ADD, MULTIPLY, ENABLE }
 @export var skill_name: String
 @export var skill_desc: String
 @export var skill_cost: int
-@export var max_level: int
 @export var affected_stat: AffectedStat
 @export var modifier_type: ModifierType
-@export var modifier_values_by_level: Array[float]
-
-
-func _init() -> void:
-	modifier_values_by_level = []
+@export var modifier_value: float
