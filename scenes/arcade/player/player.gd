b066b68bc8510b6dce14f9aabe89e45f178ef0e2
Steve Kain <stevekain@gmail.com>
2026-03-16 15:45:35 -0500

    player cleanup

--- scenes/arcade/player/player.gd
+++ scenes/arcade/player/player.gd
@@ -2,35 +2,36 @@ extends Area2D
 class_name Player
 
 signal player_hit
-signal start_game_pressed
 
 @onready var hit_player: AudioStreamPlayer2D = $HitPlayer
 @onready var camera_2d: Camera2D = $Camera2D
 @onready var cannons: Node2D = $Cannons
 @onready var player_sprite: Sprite2D = %Player
 
-
 var max_health := 3
 var health := 3
 
 func _ready() -> void:
 	reset()
-	
+
+
 func _process(_delta: float) -> void:
 	if health > 0:
 		cannons.handle_firing()
-	elif Input.is_action_pressed("start_game"):
-		start_game_pressed.emit()
-		
+
+
 func die() -> void:
 	cannons.disable_cannons()	
-	
+
+
 func reset() -> void:
 	health = max_health
 	cannons.reset_cannons()	
-	
+
+
 func make_camera_current() -> void:
 	camera_2d.make_current()
+
 
 func _handle_hit(projectile: EnemyProjectile) -> void:
 	if not health > 0:
@@ -53,6 +54,7 @@ func _hit_flash() -> void:
 		var cannon := node as Cannon
 		cannon.add_flash_to_tween(tween)
 
+
 func _shake_camera(intensity: float, duration: float) -> void:
 	var shake_tween := create_tween()
 
@@ -62,8 +64,10 @@ func _shake_camera(intensity: float, duration: float) -> void:
 		shake_tween.tween_property(camera_2d, "offset", offset, duration / 10.0).set_trans(Tween.TRANS_SINE)
 		shake_tween.tween_property(camera_2d, "offset", Vector2.ZERO, duration / 10.0).set_trans(Tween.TRANS_SINE)
 
+
 func _on_cannon_hit(_cannon_direction: Vector2) -> void:
 	_shake_camera(5.0, 0.125)
-	
+
+
 func _on_area_entered(area: Area2D) -> void:
 	_handle_hit(area)
