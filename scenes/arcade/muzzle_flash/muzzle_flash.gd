2cfe1509693768ec719585d46712f22cdd1a2c7c
skain <stevekain@gmail.com>
2026-06-27 12:47:15 -0500

    muzzle flash gpu particles to cpu particles switch renderer to gl_compatibility

--- scenes/arcade/muzzle_flash/muzzle_flash.gd
+++ scenes/arcade/muzzle_flash/muzzle_flash.gd
@@ -3,10 +3,10 @@ class_name MuzzleFlash
 
 @export var flash_color := Color.from_string("f95738", Color.WHITE)
 
-@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+@onready var cpu_particles_2d: CPUParticles2D = %CPUParticles2D
 
 func _ready() -> void:
-	gpu_particles_2d.process_material.color = flash_color * 4.0
+	cpu_particles_2d.color = flash_color
 
 func emit_flash() -> void:
-	gpu_particles_2d.emitting = true
+	cpu_particles_2d.emitting = true
