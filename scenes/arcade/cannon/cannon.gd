de41813e82532f83a226a62430286596c9a3adaf
skain <stevekain@gmail.com>
2026-07-03 17:34:11 -0500

    autofire working, needs visual tweaks and skill tree impl

--- scenes/arcade/cannon/cannon.gd
+++ scenes/arcade/cannon/cannon.gd
@@ -1,5 +1,4 @@
-extends Area2D
-class_name Cannon
+class_name Cannon extends Area2D
 
 enum CannonStates { READY_TO_FIRE, RECHARGING, DESTROYED }
 
@@ -14,12 +13,17 @@ const PROJECTILE_SCENE = preload("res://scenes/arcade/player_projectile/player_p
 @onready var muzzle_flash: MuzzleFlash = $MuzzleFlash
 @onready var radial_cooldown: RadialCooldown = %RadialCooldown
 @onready var autofire_line_2d: Line2D = %AutofireLine2D
+@onready var autofire_area_2d: Area2D = %AutofireArea2D
 
 @export var fire_cooldown_base: float = 10.0
+@export var autofire_not_detected_color : Color
+@export var laser_base_width: float = 2.0
+@export var autofire_detected_color : Color
 
 var fire_direction: Vector2
 var cur_state := CannonStates.READY_TO_FIRE
 var _autofire_enabled := false
+var _flicker_phase_offset: float = 0.0
 
 func _ready() -> void:
 	fire_direction = _get_fire_direction()
@@ -28,7 +32,28 @@ func _ready() -> void:
 		radial_cooldown.position.x *= -1.0
 
 
+
+func _process(_delta: float) -> void:
+	if _autofire_enabled:
+		_evaluate_autofire()
+		
+		# --- CHOOSE AN EFFECT CONFIGURATION ---
+		
+		# Option A: Decoupled Opacity Jitter
+		var base_color := autofire_detected_color if _has_targets_in_zone() else autofire_not_detected_color
+		var time := (Time.get_ticks_msec() * 0.05) + _flicker_phase_offset
+		var alpha_ripple := 0.7 + (sin(time) * cos(time * 1.6) * 0.3)
+		
+		autofire_line_2d.default_color = base_color
+		autofire_line_2d.default_color.a *= clamp(alpha_ripple, 0.2, 1.0)
+		
+		# Option B: Decoupled Width Jitter (If using this along with Opacity)
+		# var time_width = (Time.get_ticks_msec() * 0.04) + _flicker_phase_offset
+		# autofire_line_2d.width = laser_base_width + (sin(time_width) * cos(time_width * 1.7) * 0.
+
+
 func _setup_autofire() -> void:
+	_flicker_phase_offset = randf_range(0.0, 100.0)
 	match rotation_degrees:
 		0.0:
 			_autofire_enabled = GameManager.skills_manager.get_top_cannon_autofire_enabled()
@@ -40,8 +65,10 @@ func _setup_autofire() -> void:
 			_autofire_enabled = GameManager.skills_manager.get_left_cannon_autofire_enabled()
 		_:
 			print("Error: " + name + " has unrecognized rotation: ", rotation_degrees)
-	autofire_ray_cast_2d.enabled = _autofire_enabled
+	
 	autofire_line_2d.visible = _autofire_enabled
+	autofire_area_2d.set_deferred("monitoring", _autofire_enabled)
+	
 
 func _set_fire_cooldown() -> void:
 	var cooldown := GameManager.skills_manager.get_cannon_cooldown()
@@ -77,7 +104,8 @@ func fire_projectile(projectile_owner: Node) -> void:
 	muzzle_flash.emit_flash()
 
 	cur_state = CannonStates.RECHARGING
-	radial_cooldown.start_cooldown()	
+	radial_cooldown.start_cooldown()
+	_evaluate_autofire()
 
 
 func add_flash_to_tween(tween: Tween) -> void:
@@ -95,6 +123,29 @@ func disable() -> void:
 	cur_state = CannonStates.DESTROYED
 
 
+func _evaluate_autofire() -> void:
+	if not _autofire_enabled:
+		return
+	
+	autofire_line_2d.default_color = autofire_not_detected_color
+	
+	var targets_detected := _has_targets_in_zone()
+	
+	if targets_detected:
+		autofire_line_2d.default_color = autofire_detected_color
+		
+		if cur_state == CannonStates.READY_TO_FIRE:
+			fire_projectile(get_parent())
+
+
+func _has_targets_in_zone() -> bool:
+	for area in autofire_area_2d.get_overlapping_areas():
+		if area.is_in_group("Enemy") or area.is_in_group("EnemyProjectile"):
+			return true
+			
+	return false
+
+#region signal handlers
 func _on_area_entered(area: Area2D) -> void:
 	var projectile := area as EnemyProjectile
 	if projectile:
@@ -106,3 +157,12 @@ func _on_area_entered(area: Area2D) -> void:
 func _on_radial_cooldown_cooldown_complete() -> void:
 	if cur_state == CannonStates.RECHARGING:
 		cur_state = CannonStates.READY_TO_FIRE
+	
+	_evaluate_autofire()
+
+
+func _on_autofire_area_2d_area_entered(area: Area2D) -> void:
+	if _autofire_enabled:
+		_evaluate_autofire()
+
+#endregion
--- temp.gd
+++ temp.gd
@@ -1,6 +1,3 @@
 extends Node2D
 
-@onready var smart_bomb_screen_effect: SmartBombScreenEffect = $SmartBombScreenEffect
-
-func _ready() -> void:
-	await smart_bomb_screen_effect.trigger()
+@export var x : Color
